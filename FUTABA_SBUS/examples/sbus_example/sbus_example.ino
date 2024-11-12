f36d19edb059b5ec65e891eae38a1b7d53f74d13
kkbin505 <43392862+kkbin505@users.noreply.github.com>
2024-11-12 10:37:05 +0800

    pwm output

--- FUTABA_SBUS/examples/sbus_example/sbus_example.ino
+++ FUTABA_SBUS/examples/sbus_example/sbus_example.ino
@@ -4,10 +4,14 @@
 
 FUTABA_SBUS sBus;
 
+const int pwmPins[] = {3, 5, 6, 9};  // Array for PWM pins
 
 void setup(){
   sBus.begin();
-  Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    pinMode(pwmPins[i], OUTPUT);  // Set each pin in pwmPins array as output
+  }
+  //Serial.begin(115200);
 }
 
 void loop(){
@@ -16,6 +20,9 @@ void loop(){
     sBus.UpdateServos();
     sBus.UpdateChannels();
     sBus.toChannels = 0;
+    for (int i = 0; i < 4; i++) {
+      pwmValues[i] = sBus.channels[i] / 4;
+    }
     Serial<<sBus.channels[0]<<","<<sBus.channels[1]<<","<<sBus.channels[2]<<"\r\n";
   }
 }
